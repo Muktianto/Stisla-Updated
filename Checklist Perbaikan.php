1e3814650edeb5ede3393106e20868cf6b3a0596
Dwi Muktianto <dwi.muktianto@gmail.com>
2019-04-23 09:02:26 +0700

    Initial

---
+++ Checklist Perbaikan.php
@@ -0,0 +1,137 @@
+<?php
+pengelompokan css & js 
+
+
+<!--           CSS Files          -->
+
+<!--           General         -->  // default include
+<link rel="stylesheet" href="../assets/css/style.css">
+<link rel="stylesheet" href="../assets/css/components.css">
+<link rel="stylesheet" href="../assets/modules/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="../assets/css/fontawesome_v5.8.1.css">
+<!--           datatable         -->
+<link rel="stylesheet" href="../assets/modules/datatables/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="../assets/modules/datatables/datatables.min.css">
+<link rel="stylesheet" href="../assets/modules/datatables/select.bootstrap4.min.css">
+<!--           input         -->
+<link rel="stylesheet" href="../assets/modules/summernote/dist/summernote-bs4.css">
+<link rel="stylesheet" href="../node_modules/selectric/public/selectric.css">
+<link rel="stylesheet" href="../assets/modules/prism/prism.css"> //modal
+<link rel="stylesheet" href="../assets/modules/select2/select2.min.css">
+<!--            toast          -->
+<link rel="stylesheet" href="../assets/modules/izitoast/iziToast.min.css"> 
+
+
+<!--           advance_input         -->
+<link rel="stylesheet" href="../assets/modules/bootstrap-tagsinput/bootstrap-tagsinput.css">
+<link rel="stylesheet" href="../assets/modules/bootstrap-timepicker/bootstrap-timepicker.min.css">
+<link rel="stylesheet" href="../assets/modules/bootstrap-daterangepicker/daterangepicker.css">
+<link rel="stylesheet" href="../assets/modules/bootstrap-colorpicker/bootstrap-colorpicker.min.css">
+<!--            ionicon          -->
+<link rel="stylesheet" href="../assets/modules/ionicons/ionicons.min.css">
+<!--           image         -->
+<link rel="stylesheet" href="../assets/modules/owlcarousel2/dist/owl.theme.default.min.css">
+<link rel="stylesheet" href="../assets/modules/owlcarousel2/dist/owl.carousel.min.css">
+<link rel="stylesheet" href="../assets/modules/chocolat/dist/css/chocolat.css">
+<!--           map         -->
+<link rel="stylesheet" href="../assets/modules/jqvmap/dist/jqvmap.min.css">
+<link rel="stylesheet" href="../assets/modules/flag-icon-css/css/flag-icon.min.css">
+<!--           social         -->
+<link rel="stylesheet" href="../node_modules/bootstrap-social/bootstrap-social.css">
+<!--           weather         -->
+<link rel="stylesheet" href="../assets/modules/weathericons/css/weather-icons.min.css">
+<link rel="stylesheet" href="../assets/modules/weathericons/css/weather-icons-wind.min.css">
+<!--           multiupload         -->
+<link rel="stylesheet" href="../assets/modules/dropzone/dropzone.css">
+<!--           code_mirror         -->
+<link rel="stylesheet" href="../assets/modules/codemirror/codemirror.css">
+<link rel="stylesheet" href="../assets/modules/codemirror/duotone-dark.css">
+<!--           calendar        -->
+<link rel="stylesheet" href="../assets/modules/fullcalendar/fullcalendar.min.css">
+
+----------------------------------------------------------
+
+<!--            JS Scripts          -->
+
+<!--            General          --> // default include
+<script src="../assets/modules/jquery-3.3.1.min.js"></script>
+<script src="../assets/modules/popper.js"></script>
+<script src="../assets/modules/tooltip.js"></script>
+<script src="../assets/modules/bootstrap/js/bootstrap.min.js"></script>
+<script src="../assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
+<script src="../assets/modules/moment.min.js"></script>
+<script src="../assets/js/stisla.js"></script>
+<script src="../assets/js/scripts.js"></script>
+<script src="../assets/js/custom.js"></script>
+<!--            toast          -->
+<script src="../assets/modules/izitoast/iziToast.min.js"></script>
+<script src="../assets/modules/sweetalert/sweetalert.min.js"></script>
+<!--            Textarea/email/post          -->
+<script src="../assets/modules/summernote/summernote-bs4.js"></script>
+<script src="../node_modules/jquery-pwstrength/jquery.pwstrength.min.js"></script>
+<script src="../node_modules/selectric/public/jquery.selectric.min.js"></script>
+<script src="../assets/modules/select2/select2.full.min.js"></script>
+<script src="../assets/modules/prism/prism.js"></script>
+
+<!--           advance_input         -->
+<script src="../assets/modules/cleave/cleave.min.js"></script>
+<script src="../assets/modules/cleave/cleave-phone.us.js"></script>
+<script src="../assets/modules/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="../assets/modules/bootstrap-colorpicker/bootstrap-colorpicker.min.js"></script>
+<script src="../assets/modules/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
+<script src="../assets/modules/bootstrap-tagsinput/bootstrap-tagsinput.min.js"></script>
+<!--            chart          -->
+<script src="../assets/modules/sparkline/jquery.sparkline.min.js"></script>
+<script src="../assets/modules/chart.min.js"></script>
+<!--            map          -->
+<script src="../assets/modules/jqvmap/dist/jquery.vmap.min.js"></script>
+<script src="../assets/modules/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+<script src="../assets/modules/jqvmap/dist/maps/jquery.vmap.indonesia.js"></script>
+<!--            image          -->
+<script src="../assets/modules/owlcarousel2/dist/owl.carousel.min.js"></script> //image
+<script src="../assets/modules/chocolat/dist/js/jquery.chocolat.min.js"></script> //image preview
+<script src="../assets/modules/jquery-ui/jquery-ui.min.js"></script>
+<!--           weather         -->
+<script src="../assets/modules/simpleweather/jquery.simpleWeather.min.js"></script>
+<!--           sticky_chatbox         -->
+<script src="../assets/modules/sticky-kit/sticky-kit.js"></script>
+<script src="../assets/js/page/components-chat-box.js"></script>
+<!--           multiupload         -->
+<script src="../assets/modules/dropzone/dropzone.min.js"></script>
+<script src="../assets/modules/upload-preview/uploadPreview.min.js"></script>
+<!--           codemirror         -->
+<script src="../assets/modules/codemirror/codemirror.js"></script>
+<script src="../assets/modules/codemirror/mode/javascript.js"></script>
+<!--           calendar        -->
+<script src="../assets/modules/fullcalendar/fullcalendar.min.js"></script>
+
+//index.html
+//index-0.html
+//bootstrap-card.html
+//bootstrap-modal.html 
+//bootstrap-table.html 
+//bootstrap-typography.html
+//components-gallery.html css icons
+//components-multiple-upload.html
+//components-statistic.html
+//components-user.html
+//forms-advanced-form.html
+//forms-editor.html
+//modules-calendar.html
+//modules-chartjs.html
+//modules-datatables.html
+//modules-flag.html
+//modules-ion-icons.html
+//modules-owl-carousel.html
+//modules-sparkline.html
+//modules-sweet-alert.html
+//modules-toastr.html
+//modules-vector-map.html
+//modules-weather-icon.html
+//auth-login.html
+//features-post-create.html
+//features-profile.html
+//features-setting-detail.html
+//features-tickets.html
+//utilities-invoice.html
+
